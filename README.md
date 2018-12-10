819d6dc2abe65ce463f55fa6034bb56ac80ce98b
Anton <anton@adc.sh>
2018-12-10 13:05:43 +0300

    add src, types, doc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # @idio/facebook
 
-[![npm version](https://badge.fury.io/js/@idio/facebook.svg)](https://npmjs.org/package/@idio/facebook)
+[![npm version](https://badge.fury.io/js/%40idio%2Ffacebook.svg)](https://npmjs.org/package/@idio/facebook)
 
 `@idio/facebook` is The Facebook OAth Login Routes For The Idio Web Server.
 
@@ -12,7 +12,7 @@ yarn add -E @idio/facebook
 
 - [Table Of Contents](#table-of-contents)
 - [API](#api)
-- [`facebook(arg1: string, arg2?: boolean)`](#mynewpackagearg1-stringarg2-boolean-void)
+- [`facebook(arg1: string, arg2?: boolean)`](#facebookarg1-stringarg2-boolean-void)
   * [`Config`](#type-config)
 - [Copyright](#copyright)
 
@@ -34,10 +34,13 @@ Call this function to get the result you want.
 
 __<a name="type-config">`Config`</a>__: Options for the program.
 
-|   Name    |   Type    |    Description    | Default |
-| --------- | --------- | ----------------- | ------- |
-| shouldRun | _boolean_ | A boolean option. | `true`  |
-| __text*__ | _string_  | A text to return. | -       |
+|        Name        |             Type              |                                                                         Description                                                                          |         Default         |
+| ------------------ | ----------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------ | ----------------------- |
+| __client_id*__     | _string_                      | The app's client id.                                                                                                                                         | -                       |
+| __client_secret*__ | _string_                      | The app's client secret.                                                                                                                                     | -                       |
+| path               | _string_                      | The server path to start the login flaw and use for redirect (`${path}/redirect`).                                                                           | `/auth/facebook`        |
+| scope              | _string_                      | The scope to ask permissions for.                                                                                                                            | -                       |
+| finish             | _(ctx, token, data) =&gt; {}_ | The function to complete the authentication that receives the token and the data about the user, such as name and id. The default function redirects to `/`. | `setSession; redirect;` |
 
 ```js
 /* yarn example/ */
@@ -51,7 +54,7 @@ import facebook from '@idio/facebook'
 })()
 ```
 ```
-example
+
 ```
 
 <p align="center"><a href="#table-of-contents"><img src=".documentary/section-breaks/2.svg?sanitize=true"></a></p>
--- build/index.js
+++ build/index.js
@@ -1,30 +1,70 @@
-const { debuglog } = require('util');
+const { facebookDialogUrl, exchange, graphGet } = require('@demimonde/graph');
 
-const LOG = debuglog('@idio/facebook')
+const getRedirect = (ctx, path) => {
+  return `${ctx.protocol}://${ctx.host}${path}/redirect`
+}
 
 /**
- * The Facebook OAth Login Routes For The Idio Web Server.
+ * The Facebook OAth Login Routes For The Idio Web Server. Requires the session middleware to be installed. The default path is `/auth/facebook` but can be changed.
+ * @param {string} [router] The router instance.
  * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * @param {string} config.client_id The app's client id.
+ * @param {string} config.client_secret The app's client secret.
+ * @param {string} [config.path="/auth/facebook"] The app's client secret. Default `/auth/facebook`.
+ * @param {string} [config.scope] The scope to ask permissions for.
+ * @param {(ctx, token, data) => {}} [config.finish="setSession; redirect;"] The function to complete the authentication that receives the token and the data about the user, such as name and id. The default function redirects to `/`. Default `setSession; redirect;`.
  */
-               async function facebook(config = {}) {
+export default async function facebook(router, config = {}) {
   const {
-    shouldRun = true,
-    text,
+    client_id,
+    client_secret,
+    path = '/auth/facebook',
+    scope,
+    finish = (ctx, token, user) => {
+      ctx.session.token = token
+      ctx.session.user = user
+      ctx.redirect('/')
+    },
   } = config
-  if (!shouldRun) return
-  LOG('@idio/facebook called with %s', text)
-  return text
+
+  router.get(path, async (ctx) => {
+    const state = Math.floor(Math.random() * 10000)
+    ctx.session.state = state
+    const redirect_uri = getRedirect(ctx)
+    const u = facebookDialogUrl({
+      redirect_uri,
+      client_id,
+      scope,
+      state,
+    })
+    ctx.redirect(u)
+  })
+  router.get(`${path}/redirect`, async (ctx) => {
+    const redirect_uri = getRedirect(ctx)
+    const state = ctx.query.state
+    if (state != ctx.session.state) {
+      throw new Error('The state is incorrect.')
+    }
+    ctx.session.state = null
+    if (!ctx.query.code) throw new Error('Code Not Found.')
+
+    const token = await exchange({
+      client_id,
+      client_secret,
+      redirect_uri,
+      code: ctx.query.code,
+    })
+    const data = await graphGet('/me', token, {}, true)
+    await finish(ctx, token, data)
+  })
 }
 
 /* documentary types/index.xml */
 /**
  * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
+ * @prop {string} client_id The app's client id.
+ * @prop {string} client_secret The app's client secret.
+ * @prop {string} [path="/auth/facebook"] The app's client secret. Default `/auth/facebook`.
+ * @prop {string} [scope] The scope to ask permissions for.
+ * @prop {(ctx, token, data) => {}} [finish="setSession; redirect;"] The function to complete the authentication that receives the token and the data about the user, such as name and id. The default function redirects to `/`. Default `setSession; redirect;`.
  */
-
-
-module.exports = facebook
-//# sourceMappingURL=index.js.map
